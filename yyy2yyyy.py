9c8bc95499cdb3943c71220c20bea2a7c1f7a18a
Andrew Chen <yongjhih@gmail.com>
2021-04-11 22:14:56 +0800

    Refine

--- yyy2yyyy.py
+++ yyy2yyyy.py
@@ -4,6 +4,11 @@ import sys
 import pandas as pd
 
 
+def yyy2yyyy(yyymmdd, delimiter="/"):
+    parts = yyymmdd.split(delimiter)
+    year = int(parts[0]) + 1911 + (100 if (parts[0].startswith("0")) else 0)
+    return f"{year}-{parts[1]}-{parts[2]}"
+
 def main(args):
     if not args[0]:
         sys.exit(1)
@@ -14,12 +19,16 @@ def main(args):
     df.drop('Unnamed: 0', axis=1, inplace=True)
     df.drop(df.filter(regex="Unnamed"),axis=1, inplace=True)
 
+    d = df['手術日期']
+    for i in range(len(df)):
+        d.iloc[i]=yyy2yyyy(d.iloc[i])
+    pd.to_datetime(d, format='%Y-%m-%d')
+
     d = df['報告日期']
     for i in range(len(df)):
-        d.iloc[i]=d.iloc[i].replace(d.iloc[i][0:3], str(int(d.iloc[i][0:3]) + 1911))
-    d=pd.to_datetime(d,format='%Y/%m/%d')
-    #df['報告日期'] = pd.to_datetime(df['報告日期'])
-    #df['報告日期'] = pd.to_datetime(df['報告日期'])
+        d.iloc[i]=yyy2yyyy(d.iloc[i])
+    pd.to_datetime(d, format='%Y-%m-%d')
+
     df.to_csv(args[0])
 
 
